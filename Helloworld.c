b124de530702a4dbec9517d3eda495603c47c9bc
Durdana Sultana <165191846+durdana3105@users.noreply.github.com>
2024-10-12 14:08:41 +0530

    Add files via upload

---
+++ Helloworld.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main(){
+  printf("hello\n");
+  for(i=0)
+}
+/*#include<stdio.h>
+
+  int main() {
+   float fahrenheit,celsius;
+
+  
+
+printf("Enter the temperature in fahrenheit : ");
+    scanf("%f", &fahrenheit);
+    celsius = (fahrenheit - 32)* 5/9;
+    printf("celsius : %f",celsius);
+    return 0;
+  }*/
+
+ 
+
+
+
+    
+   
+
+
+
+
+   
